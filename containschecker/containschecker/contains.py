a8b82e3493be3c7dd9b0dc6249f0e805dc7f711a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-09 14:38:58 -0500

    Add the contains.py file with the appropriate TODO markers.

---
+++ containschecker/containschecker/contains.py
@@ -0,0 +1,4 @@
+# TODO: Add all of the functions that you need to perform the specified tasks
+
+# TODO: Make sure that you define at least two functions and that they
+# are commented in a suitable fashion with both multiple-line and single-line comments
